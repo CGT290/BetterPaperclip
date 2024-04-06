8e1dcf3cd97b7ee1c25b10b784f48515c87fcdd7
Peter Joseph <96744676+CGT290@users.noreply.github.com>
2024-04-05 22:05:41 -0400

    Update login.js
    Copied over

--- LoginPage/login.js
+++ LoginPage/login.js
@@ -1,1 +1,34 @@
+document.addEventListener('DOMContentLoaded', function(){
+       
+    const loginButton = document.getElementById('LoginButton');
+    const userName = document.getElementById('username');
+    const password = document.getElementById('password');
+    const passwordError = document.getElementById('passwordErrors');
+    const userNameError = document.getElementById('userNameErrors');
+    
 
+        loginButton.addEventListener('click', function(a){
+            userNameError.textContent = '';
+            passwordError.textContent = '';
+
+            const notEmpty = true;
+
+            if(userName.value.trim() === ''){
+                a.preventDefault();
+                userNameError.textContent = 'Please Input your ID';
+                notEmpty = false;
+
+            }
+            if(password.value.trim() === ''){
+                a.preventDefault();
+                passwordError.textContent = 'Please Input your Password';
+                notEmpty = false;
+
+            }
+        })
+
+
+
+
+
+});
