0aa4cfb51ec6fa18caa4defd4edbb9087424f620
Johnathansanchez16 <150489054+Johnathansanchez16@users.noreply.github.com>
2024-04-07 00:06:56 -0400

    updated and fixed upload print option section

--- UploadPage/script.js
+++ UploadPage/script.js
@@ -1,3 +1,9 @@
 function updateButtonText(option, element) {
   element.closest('.dropdown').querySelector('.dropdown-toggle').innerText = option;
+  if(option=='Custom'){
+      element.closest('.dropdown').querySelector('.custom-page').classList.toggle("cust-display");
+  }
+  else{
+      element.closest('.dropdown').querySelector('.custom-page').classList.remove("cust-display");
+  }
 }
