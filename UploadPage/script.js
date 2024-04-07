5fe9ecf481223e2f1d5aa7b3b083414947b86f7a
Johnathansanchez16 <150489054+Johnathansanchez16@users.noreply.github.com>
2024-04-07 12:35:39 -0400

    fixed custom selection

--- UploadPage/script.js
+++ UploadPage/script.js
@@ -1,10 +1,9 @@
-function updateButtonText(option, element) {
-  element.closest('.dropdown').querySelector('.dropdown-toggle').innerText = option;
+function customTextBar(element, option) {
   if(option=='Custom'){
-      element.closest('.dropdown').querySelector('.custom-page').classList.toggle("cust-display");
+    element.closest('.mb-3').querySelector('.custom-text-bar').classList.toggle("cust-display");
   }
   else{
-      element.closest('.dropdown').querySelector('.custom-page').classList.remove("cust-display");
+    element.closest('.mb-3').querySelector('.custom-text-bar').classList.toggle("custom-text-bar");
   }
 }
 
