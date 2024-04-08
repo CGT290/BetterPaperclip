db78b0f7018d7e518f577f97b4c019162f5e9034
Johnathansanchez16 <150489054+Johnathansanchez16@users.noreply.github.com>
2024-04-07 20:14:00 -0400

    fix error

--- UploadPage/script.js
+++ UploadPage/script.js
@@ -12,67 +12,65 @@ function customTextBar(selectElement) {
   document.querySelector(".drop-area").setAttribute("style", "height: 600px;");
   }
 }
-
-
 document.querySelectorAll(".drop-area_input").forEach(inputElement => {
-  const dropArea = inputElement.closest(".drop-area");
+const dropArea = inputElement.closest(".drop-area");
 
-  dropArea.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    
-    dropArea.classList.add('active');
-  });
+dropArea.addEventListener("dragover", (e) => {
+  e.preventDefault();
 
-  dropArea.addEventListener("dragleave", (e) => {
-    
-    dropArea.classList.remove('active');
-  });
+  dropArea.classList.add('active');
+});
 
-  dropArea.addEventListener("drop", (e) => {
-    e.preventDefault();
-    
-    dropArea.classList.remove('active');
+dropArea.addEventListener("dragleave", (e) => {
 
-    const files = e.dataTransfer.files;
-    if (files.length) {
-      updateDropArea(files[0], dropArea); 
-    }
-  });
-  dropArea.addEventListener("click", (e) => {
+  dropArea.classList.remove('active');
+});
 
-    inputElement.click();
+dropArea.addEventListener("drop", (e) => {
+  e.preventDefault();
+
+  dropArea.classList.remove('active');
+
+  const files = e.dataTransfer.files;
+  if (files.length) {
+    updateDropArea(files[0], dropArea);
+  }
+});
+dropArea.addEventListener("click", (e) => {
+
+  inputElement.click();
 });
 inputElement.addEventListener("change", (e) => {
-    const files = e.target.files;
-    if (files.length) {
-        updateDropArea(files[0], dropArea);
-    }
+  const files = e.target.files;
+  if (files.length) {
+      updateDropArea(files[0], dropArea);
+  }
 });
 
 });
 
 function updateDropArea(file, dropArea) {
-  if (file.type.startsWith('image/')) {
-   
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      dropArea.innerHTML = '';
-      const img = new Image();
-      img.src = e.target.result;
-      
-       
-      dropArea.appendChild(img); 
-    };
-    reader.readAsDataURL(file);
-  } else {
-   
-    dropArea.innerHTML = `<p>File name: ${file.name}</p>`;
-  }
+if (file.type.startsWith('image/')) {
+
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const img = new Image();
+    img.src = e.target.result;
+    img.style.width = '100%';
+    img.style.height = 'auto';
+    img.style.objectFit = 'contain';
+    dropArea.innerHTML = '';
+    dropArea.appendChild(img);
+  };
+  reader.readAsDataURL(file);
+} else {
+
+  dropArea.innerHTML = `<p>File name: ${file.name}</p>`;
+}
 }
 function Upload(element) {
-  var image = element.closest(".side-containder").querySelector("img");
-  var previewContainer = document.querySelector("#queue-preview .preview-container");
-  previewContainer.appendChild(image.cloneNode(true));
+var image = element.closest(".side-containder").querySelector("img");
+var previewContainer = document.querySelector("#queue-preview .preview-container");
+previewContainer.appendChild(image.cloneNode(true));
 }
-
 
